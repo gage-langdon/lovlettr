4894c21c34d909459a5d3555a5ab3f21840d5f5d
Gage Langdon <glangdon@gmail.com>
2019-03-17 22:11:34 -0700

    chore: update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `LoveLttr`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    description: `Uniquely Personalized, interactive love letters`,
+    author: `LoveLttr`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
