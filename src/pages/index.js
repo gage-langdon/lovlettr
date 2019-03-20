fa6d2a05701e37efd57050326100ae0a462e6616
Gage Langdon <gagelangdon@gmail.com>
2019-03-19 23:34:04 -0700

    wip: home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import { Link } from 'gatsby';
 import Button from '../components/button';
 import Layout from '../components/layout';
 import Label from '../components/label';
+
+// Images
 import CoupleIllustration1 from '../images/couple-illustration-1.svg';
+import SendAndReceiveIllustration from '../images/recieve-and-send-illustration.svg';
 
 const IndexPage = () => (
   <Layout>
@@ -12,8 +15,21 @@ const IndexPage = () => (
         <h2 className="h1">Uniquely Personalized, Interactive Love Letters</h2>
       </Label>
     </div>
-    <div className="text-center">
+    <div className="d-flex flex-column align-items-center">
       <CoupleIllustration1 />
+      <Link
+        to="/compose"
+        className="mt-3"
+        style={{ paddingLeft: '150px', bottom: '4' }}
+      >
+        <Button primary text="Try it out" />
+      </Link>
+    </div>
+    <div className="d-flex flex-column align-items-center pt-5 mt-5">
+      <SendAndReceiveIllustration />
+      <Label cursive className="pt-4">
+        <h3>Receive and send a response</h3>
+      </Label>
     </div>
     <div className="pt-5">
       <div className="d-flex flex-column align-items-center pt-5">
