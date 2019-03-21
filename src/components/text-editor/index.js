5fd207f4af22080df336a71d45165f12403c2df2
Gage Langdon <gagelangdon@gmail.com>
2019-03-20 18:59:46 -0700

    chore: move html strip to inside of textEditor

--- src/components/text-editor/index.js
+++ src/components/text-editor/index.js
@@ -16,13 +16,25 @@ class TextEditor extends React.Component {
     // Set placeholder if exists
     if (!value && placeholder && onChange) onChange(placeholder);
   }
+  onChange(strVal = '') {
+    // Empty text editor still has some html scaffolding so we must clean it out in order to check if empty
+    const isLetterEmpty = !Boolean(strVal.replace(/<\/?[^>]+(>|$)/g, ''));
 
+    // Logic outside of this component may rely on empty value so its
+    // best to send back empty from here instead of filtering html outside
+    if (isLetterEmpty) strVal = '';
+    this.props.onChange(strVal);
+  }
   render() {
-    const { value, onChange } = this.props;
+    const { value } = this.props;
     return (
       <div className=" d-flex flex-column">
         <div className="p-3">
-          <ReactQuill theme="bubble" value={value} onChange={onChange} />
+          <ReactQuill
+            theme="bubble"
+            value={value}
+            onChange={val => this.onChange(val)}
+          />
         </div>
       </div>
     );
