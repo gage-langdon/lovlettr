23394a2c7e1f7705b18c1cf98b8b48dc202b4360
Maxton Delaney <maxtondelaney@gmail.com>
2019-04-29 14:23:44 -0700

    init commit, used axios for post and get

---
+++ src/utilities/https.js
@@ -0,0 +1,25 @@
+/*
+endpoints:
+  POST - https://vph540u747.execute-api.us-east-1.amazonaws.com/dev/ltr/create
+  GET - https://vph540u747.execute-api.us-east-1.amazonaws.com/dev/ltr/{id}
+*/
+const axios = require('axios');
+
+export const get = async id => {
+  const response = await axios.get(
+    `https://vph540u747.execute-api.us-east-1.amazonaws.com/dev/ltr/${id}`
+  );
+  return response;
+};
+
+export const post = async ({ email, letterHTML }) => {
+  const data = {
+    body: letterHTML,
+    email: email
+  };
+  const response = await axios.post(
+    'https://vph540u747.execute-api.us-east-1.amazonaws.com/dev/ltr/create',
+    data
+  );
+  console.log(response);
+};
