8dbddc5cb3fd69ee26cad1d2279139e0890fbbb4
Maxton Delaney <maxtondelaney@gmail.com>
2019-06-06 09:32:21 -0700

    fixed test

--- src/components/sub/.spec.js
+++ src/components/sub/.spec.js
@@ -5,7 +5,7 @@ import Sub from './index';
 
 describe('Sub component', () => {
   it('Will render with just text passed in', () => {
-    const wrap = shallow(<AddAnswer text="test" />);
+    const wrap = shallow(<Sub text="test" />);
     expect(wrap).toMatchSnapshot();
   });
 });
