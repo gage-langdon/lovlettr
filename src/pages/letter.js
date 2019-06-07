56b9c824c991f4aa6e0998ef7e61f7518399a6d4
Maxton Delaney <maxtondelaney@gmail.com>
2019-06-07 15:11:27 -0700

    added redirects and alternate return

--- src/pages/letter.js
+++ src/pages/letter.js
@@ -2,24 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 //utils
 import { getLtr, editLtr } from '../utilities/https';
+import { redirect } from '../utilities/location';
 
 //components
 import Button from '../components/button';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 //images
 import Pages from '../images/pages.svg';
 import Box from '../images/question-box.svg';
-import { redirect } from '../utilities/location';
+import Couple from '../images/couple-illustration-1.svg';
 
-const LettrPage = () => {
+const LettrPage = props => {
+  console.log(props);
   const [ltr, setLtr] = useState();
+
+  useEffect(() => {
+    awaitLtr('37c1abb0-8819-11e9-b48e-d55d1f5ca6cf');
+  });
 
   async function awaitLtr(ltrId) {
     if (!ltr) {
-      const ltr = await getLtr(ltrId);
-      setLtr(ltr.data);
+      const { data, status } = await getLtr(ltrId);
+      if (status !== 200) {
+        redirect('/404');
+      }
+      setLtr(data);
     }
   }
 
@@ -31,38 +39,42 @@ const LettrPage = () => {
     }
   }
 
-  useEffect(() => {
-    awaitLtr('37c1abb0-8819-11e9-b48e-d55d1f5ca6cf');
-  });
-
-  console.log(ltr);
-  const { questionInputText, answers, letterHTML, letterId } = ltr;
-
-  return (
-    <Layout>
-      <div className="pages-container">
-        <div
-          className="text-preview-field"
-          dangerouslySetInnerHTML={{ __html: letterHTML }}
-        />
-        <Pages className="pages" />
-      </div>
-      <div>
-        <div className="question-container">{questionInputText}</div>
-        <div className="answer-container">
-          {answers.map(item => (
-            <Button
-              className="button-square"
-              onClick={() => sendResponse({ questionResponse: item.text })}
-              key={item.id}
-              text={item.text}
-            />
-          ))}
+  if (ltr) {
+    const { questionInputText, answers, letterHTML } = ltr;
+    return (
+      <Layout>
+        <div className="pages-container">
+          <div
+            className="text-preview-field"
+            dangerouslySetInnerHTML={{ __html: letterHTML }}
+          />
+          <Pages className="pages" />
         </div>
-        <Box className="box" />
-      </div>
-    </Layout>
-  );
+        <div>
+          <div className="question-container">{questionInputText}</div>
+          <div className="answer-container">
+            {answers.map(item => (
+              <Button
+                className="button-square"
+                onClick={() => sendResponse({ questionResponse: item.text })}
+                key={item.id}
+                text={item.text}
+              />
+            ))}
+          </div>
+          <Box className="box" />
+        </div>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <div className="d-flex justify-content-center">
+          <Couple />
+        </div>
+      </Layout>
+    );
+  }
 };
 
 export default LettrPage;
