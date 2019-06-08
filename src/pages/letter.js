7a72e84056e8e2d81ced1d1086244d31f0bbb23b
Maxton Delaney <maxtondelaney@gmail.com>
2019-06-07 20:43:15 -0700

    letter page styled

--- src/pages/letter.js
+++ src/pages/letter.js
@@ -7,11 +7,15 @@ import { redirect } from '../utilities/location';
 //components
 import Button from '../components/button';
 import Layout from '../components/layout';
+import Sub from '../components/sub';
 
 //images
 import Pages from '../images/pages.svg';
 import Box from '../images/question-box.svg';
 import Couple from '../images/couple-illustration-1.svg';
+
+//styles
+import './styles/letter.css';
 
 const LettrPage = props => {
   const [ltr, setLtr] = useState();
@@ -44,27 +48,36 @@ const LettrPage = props => {
     const { questionInputText, answers, letterHTML } = ltr;
     return (
       <Layout>
-        <div className="pages-container">
-          <div
-            className="text-preview-field"
-            dangerouslySetInnerHTML={{ __html: letterHTML }}
-          />
-          <Pages className="pages" />
-        </div>
-        <div>
-          <div className="question-container">{questionInputText}</div>
-          <div className="answer-container">
-            {answers.map(item => (
-              <Button
-                className="button-square"
-                onClick={() => sendResponse({ questionResponse: item.text })}
-                key={item.id}
-                text={item.text}
-              />
-            ))}
+        <div className="d-flex justify-content-center">
+          <div className="pages-container">
+            <div
+              className="text-preview-field"
+              dangerouslySetInnerHTML={{ __html: letterHTML }}
+            />
+            <Pages className="pages" />
           </div>
-          <Box className="box" />
         </div>
+        <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center">
+            <div className="question-container-letter">{questionInputText}</div>
+            <div className="answer-container-letter">
+              {answers.map(item => (
+                <Button
+                  className="button-square"
+                  onClick={() => sendResponse({ questionResponse: item.text })}
+                  key={item.id}
+                  text={item.text}
+                />
+              ))}
+            </div>
+            <Box hidden={answers.length === 0} className="box-letter" />
+          </div>
+        </div>
+        <Sub
+          className="d-flex justify-content-center mt-1 mb-3"
+          text="click an answer and the sender is notified"
+          hidden={answers.length === 0}
+        />
       </Layout>
     );
   } else {
